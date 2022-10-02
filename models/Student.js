14a6f775a2fc05a114ec3e19df0f3d85774d4b0e
Adil Saju <adilsaju@gmail.com>
2022-10-02 14:52:17 -0700

    mongodb configure

---
+++ models/Student.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose')
+// mongoose.pluralize(null);
+const studentRequirementsSchema = new mongoose.Schema({
+   flownHours: Number,
+   balance: Number,
+   licenseType: String,
+   englishProficiency: Boolean,
+
+})
+
+const studentDocumentVerificationSchema = new mongoose.Schema({
+   medicalLicense: String,
+   radioLicense: String,
+   license: String,
+   uploadedDate:  {
+      type: Date,
+      default: Date.now
+   },
+   isApproved: {
+      type: Boolean,
+      default: false
+   },
+   dateOfVerification: {
+      type: Date,
+      default: Date.now
+   },
+   isReuploadRequested: {
+      type: Boolean,
+      default: false
+   },
+   // student: studentSchema,
+   // approvedAdmin: adminSchema,
+
+})
+
+const requestSchema = new mongoose.Schema({
+   flightDate: {
+      type: Date,
+      default: Date.now
+   },
+   isApproved: {
+      type: Boolean,
+      default: false
+   },
+   requestedDate:  {
+      type: Date,
+      default: Date.now
+   },
+   // student: studentSchema,
+   // approvedAdmin: adminSchema,
+   adminVerifiedDate: {
+      type: Date,
+      default: Date.now
+   }
+})
+
+
+
+const adminSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   approvedRequests: [requestSchema],
+   approvedStudentDocuments: [studentDocumentVerificationSchema]
+
+})
+
+const studentSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   studentNumber: Number,
+   photo: String,
+   dateJoined: {
+      type: Date,
+      default: Date.now
+   },
+   program: String,
+   studentRequirements: studentRequirementsSchema,
+   isRequirementsOk: {
+      type: Boolean,
+      default: false
+   },
+   requests: [requestSchema],
+   studentDocumentVerification: studentDocumentVerificationSchema,
+
+
+})
+
+
+
+module.exports = {
+   studentRequirements: mongoose.model('studentRequirements', studentRequirementsSchema, 'studentRequirements'),
+   studentDocumentVerification: mongoose.model('studentDocumentVerification', studentDocumentVerificationSchema, 'studentDocumentVerification'),
+   requestSchema: mongoose.model('request', requestSchema, 'request'),
+   admin: mongoose.model('admin', adminSchema, 'admin'),
+   student: mongoose.model('student', studentSchema, 'student'),
+
+
+}
--- server.js
+++ server.js
@@ -1,4 +1,59 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
-const port = 5000
+const mongoose = require('mongoose')
+const Student = require("./models/Student.js")
+
+const port = 5001
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true } )
+const db = mongoose.connection 
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.error('connected to database'))
+
+async function run() {
+
+
+    const studentRequirements = await Student.studentRequirements.create({flownHours: 123 , balance: 66, licenseType: "cpl", englishProficiency: true})
+
+    const studentDocumentVerification = await Student.studentDocumentVerification.create({medicalLicense: "43243" , radioLicense: "54365", license: "12212"})
+
+    const request = await Student.requestSchema.create({})
+    const request2 = await Student.requestSchema.create({})
+
+
+    const admin = await Student.admin.create({name: "Claire" , email: "claire@gmail.com", password: "12345678" , approvedRequests: [request,request2], approvedStudentDocuments: [studentDocumentVerification]})
+
+    const student = await Student.student.create({name: "Adill" , email: "adilsaju@gmail.com", password: "12345678", studentNumber: 35209583920, photo: "assffsafsafsa", program: "cpl", studentRequirements: studentRequirements,  requests: [request], studentDocumentVerification: studentDocumentVerification })
+    // user.name = ""
+    // await user.save()
+
+    //const user  = new User({ name: "Kyle" , age: 26 })
+    //await user.save()
+}
+
+run() 
+
+//middleware
+app.use(express.json())
+//router 
+// const subscribersRouter = require('./routes/s')
+const router = express.Router()
+//getting all
+app.get('/',async (req,res)=>{
+    try {
+        const abc=await Student.student.find()
+        res.json(abc)
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+    // res.send('hw')
+})
+
+
+app.listen(port, ()=>{
+    console.log('server started');
+})
+
+
 
