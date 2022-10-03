358370232776c9cdad042ea1350b7e920c32ffd4
Adil Saju <adilsaju@gmail.com>
2022-10-03 12:21:58 -0700

    Firebase Conncected YAYYY (app.js and index.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/Login';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+
+// import firebase from 'firebase/compat/app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpGHO4mWc03HUiq3NCFbDzcZbLfH-YFZA",
+  authDomain: "yoke-e05d7.firebaseapp.com",
+  projectId: "yoke-e05d7",
+  storageBucket: "yoke-e05d7.appspot.com",
+  messagingSenderId: "57509978133",
+  appId: "1:57509978133:web:b5486f62e1a8675a61ee1c",
+  measurementId: "G-PSY32G7NGY"
+};
+
 function App() {
+  // const firebaseApp = firebase.apps[0];
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth(app);
+
 
   return (
-    <>
-    <Login />
-    </>
-  )
+
+    <div>
+
+
+
+      <h1>React & Firebase</h1>
+      <h2>By @farazamiruddin</h2>
+      <code>
+        {/* <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre> */}
+        <pre>{JSON.stringify(app.options, null, 2)}</pre>
+        
+      </code>
+      <button id="signin">asd</button>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import firebase from "firebase/app";
+
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyCpGHO4mWc03HUiq3NCFbDzcZbLfH-YFZA",
+  authDomain: "yoke-e05d7.firebaseapp.com",
+  projectId: "yoke-e05d7",
+  storageBucket: "yoke-e05d7.appspot.com",
+  messagingSenderId: "57509978133",
+  appId: "1:57509978133:web:b5486f62e1a8675a61ee1c",
+  measurementId: "G-PSY32G7NGY"
+};
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
