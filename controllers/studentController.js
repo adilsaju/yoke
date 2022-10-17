313def97909f1ec1781178bae5055bdd56f5a750
Adil Saju <adilsaju@gmail.com>
2022-10-17 10:28:54 -0700

    controller refactoorrrr

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -21,5 +21,119 @@ const getStudents = ()=>{
 
 
 
+const getStudentById =()=>{
 
-module.exports = getStudents
+return async (req,res,next)=>{
+    console.log("hq")
+    try {
+        const abc=await Student.studentModel.findById(req.params.id)
+        res.json(abc)
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+}
+} 
+
+
+
+const getRequestsByStudentId = () => {
+    return async (req,res,next)=>{
+        try {
+            const studentId = req.query.student
+            //get requests column from student
+            // const abc=await Student.studentModel.findById(studentId).select('requests')
+            console.log("rrrr")
+            const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
+            
+            res.json(abc)
+        } catch (error) {
+            res.status(500).json({ error:true, message: error.message})
+        }
+    }
+}
+
+
+
+const postRequestByStudentId = () => {
+    return async (req,res,next)=>{
+        /*
+        {
+            "flightDate" : "2022-11-20T07:47:06.937Z",
+            "studentId": "634c84017abbf81281febf50"
+        }
+        
+        */
+            try {
+                console.log("posttttt")
+                console.log(req.body)
+        
+                const sas = await Student.studentModel.findById(req.body.studentId)
+        
+                console.log("sas",sas)
+                //create obj
+                const request = {
+                    //TODO: add date validation
+                    //TODO: if same student  and same date requests return error!!!!!!!!
+                    //TODO: if student exceeds max quota of requests, return error!!!!!!
+                    flightDate : req.body.flightDate,
+                    requestedDate: new Date(),
+                    adminVerifiedDate: null,
+                    isApproved: false,
+                    isRejected: false,
+                    isExpired: false,
+                    requestedStudent: sas,
+                    approvedAdmin: null
+                }
+                //update in db
+                const request1 = await Request.requestModel.create(request)
+                console.log("request1:",request1)
+        
+                //get same data from db
+        
+                //response that data
+        
+        
+                // const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
+                
+                res.json(request1)
+            } catch (error) {
+                res.status(500).json({ error:true, message: error.message})
+            }
+        }
+}
+
+
+
+const getRequests =()=>{
+    return async (req,res,next)=>{
+        try {
+            const abc=await Request.requestModel.find()
+            res.json(abc)
+        } catch (error) {
+            res.status(500).json({ message: error.message})
+        }
+    }
+}
+
+const getRequestById = ()=>{
+    return async (req,res,next)=>{
+        try {
+            //get requests column from student
+            const abc=await Request.requestModel.findById(req.params.id)
+            res.json(abc)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
+}
+
+
+module.exports = {getStudents:getStudents, getStudentById:getStudentById,
+     getRequestsByStudentId:getRequestsByStudentId,
+
+postRequestByStudentId:postRequestByStudentId,
+
+getRequests:getRequests,
+getRequestById: getRequestById
+
+}
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -2,103 +2,26 @@ const express = require('express')
 const router = express.Router()
 const Student = require("../models/StudentModel.js")
 const Request = require("../models/requestModel.js")
-const getStudents = require("../controllers/studentController.js")
+const {getStudents,getStudentById,getRequestsByStudentId, postRequestByStudentId, getRequests, getRequestById} = require("../controllers/studentController.js")
 
 //getting all students
 router.route('/students').get(getStudents())
 
 
 //getting particular student by id
-router.route('/students/:id').get(async (req,res,next)=>{
-    try {
-        const abc=await Student.studentModel.findById(req.params.id)
-        res.json(abc)
-    } catch (error) {
-        res.status(500).json({ message: error.message})
-    }
-})
+router.route('/students/:id').get(getStudentById())
 
 
-// //getting all requests
-// router.get('/requests',async (req,res,next)=>{
-//     try {
-//         const abc=await Request.requestModel.find()
-//         res.json(abc)
-//     } catch (error) {
-//         res.status(500).json({ message: error.message})
-//     }
-// })
-
-
-// //getting request by id
-// router.get('/requests/:id',async (req,res,next)=>{
-//     try {
-//         //get requests column from student
-//         const abc=await Request.requestModel.findById(req.params.id)
-//         res.json(abc)
-//     } catch (error) {
-//         res.status(500).json({ message: error.message })
-//     }
-// })
+//getting all requests
+// router.get('/requests',getRequests())
+//getting request by id
+// router.get('/requests/:id',getRequestById())
 
 //getting all requests by a particular student
 // url: /requests?student=634c84017abbf81281febf50
 //FOR travel order page in student UI
-router.route('/requests').get(async (req,res,next)=>{
-    try {
-        const studentId = req.query.student
-        //get requests column from student
-        // const abc=await Student.studentModel.findById(studentId).select('requests')
-        console.log("rrrr")
-        const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
-        
-        res.json(abc)
-    } catch (error) {
-        res.status(500).json({ error:true, message: error.message})
-    }
-}).post(async (req,res,next)=>{
-/*
-{
-    "flightDate" : "2022-11-20T07:47:06.937Z",
-    "studentId": "634c84017abbf81281febf50"
-}
-
-*/
-    try {
-        console.log("posttttt")
-        console.log(req.body)
-
-        const sas = await Student.studentModel.findById(req.body.studentId)
-
-        console.log("sas",sas)
-        //create obj
-        const request = {
-            //TODO: add date validation
-            flightDate : req.body.flightDate,
-            requestedDate: new Date(),
-            adminVerifiedDate: null,
-            isApproved: false,
-            isRejected: false,
-            isExpired: false,
-            requestedStudent: sas,
-            approvedAdmin: null
-        }
-        //update in db
-        const request1 = await Request.requestModel.create(request)
-        console.log("request1:",request1)
-
-        //get same data from db
-
-        //response that data
-
-
-        // const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
-        
-        res.json(request1)
-    } catch (error) {
-        res.status(500).json({ error:true, message: error.message})
-    }
-})
+router.route('/requests').get(getRequestsByStudentId())
+.post(postRequestByStudentId())
 
 
 
