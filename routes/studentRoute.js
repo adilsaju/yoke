0ddb8afe9e59e6cea247ee4d8320a928d9316d61
Adil Saju <adilsaju@gmail.com>
2022-10-17 00:55:01 -0700

    request TO api DONEEE

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -5,7 +5,7 @@ const Request = require("../models/requestModel.js")
 
 
 //getting all students
-router.get('/students',async (req,res)=>{
+router.get('/students',async (req,res,next)=>{
     try {
         const abc=await Student.studentModel.find()
         res.json(abc)
@@ -16,7 +16,7 @@ router.get('/students',async (req,res)=>{
 
 
 //getting particular student by id
-router.get('/students/:id',async (req,res)=>{
+router.get('/students/:id',async (req,res,next)=>{
     try {
         const abc=await Student.studentModel.findById(req.params.id)
         res.json(abc)
@@ -27,7 +27,7 @@ router.get('/students/:id',async (req,res)=>{
 
 
 // //getting all requests
-// router.get('/requests',async (req,res)=>{
+// router.get('/requests',async (req,res,next)=>{
 //     try {
 //         const abc=await Request.requestModel.find()
 //         res.json(abc)
@@ -38,10 +38,10 @@ router.get('/students/:id',async (req,res)=>{
 
 
 // //getting request by id
-// router.get('/requests/:id',async (req,res)=>{
+// router.get('/requests/:id',async (req,res,next)=>{
 //     try {
 //         //get requests column from student
-//         const abc=await Request.requestModel.find(req.params.id)
+//         const abc=await Request.requestModel.findById(req.params.id)
 //         res.json(abc)
 //     } catch (error) {
 //         res.status(500).json({ message: error.message })
@@ -51,7 +51,7 @@ router.get('/students/:id',async (req,res)=>{
 //getting all requests by a particular student
 // url: /requests?student=634c84017abbf81281febf50
 //FOR travel order page in student UI
-router.get('/requests',async (req,res)=>{
+router.route('/requests').get(async (req,res,next)=>{
     try {
         const studentId = req.query.student
         //get requests column from student
@@ -61,7 +61,51 @@ router.get('/requests',async (req,res)=>{
         
         res.json(abc)
     } catch (error) {
-        res.status(500).json({ message: error.message})
+        res.status(500).json({ error:true, message: error.message})
+    }
+}).post(async (req,res,next)=>{
+/*
+{
+    "flightDate" : "2022-11-20T07:47:06.937Z",
+    "studentId": "634c84017abbf81281febf50"
+}
+
+*/
+
+
+    try {
+        console.log("posttttt")
+        console.log(req.body)
+
+        const sas = await Student.studentModel.findById(req.body.studentId)
+
+        console.log("sas",sas)
+        //create obj
+        const request = {
+            //TODO: 
+            flightDate : req.body.flightDate,
+            requestedDate: new Date(),
+            adminVerifiedDate: null,
+            isApproved: false,
+            isRejected: false,
+            isExpired: false,
+            requestedStudent: sas,
+            approvedAdmin: null
+        }
+        //update in db
+        const request1 = await Request.requestModel.create(request)
+        console.log("request1:",request1)
+
+        //get same data from db
+
+        //response that data
+
+
+        // const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
+        
+        res.json(request1)
+    } catch (error) {
+        res.status(500).json({ error:true, message: error.message})
     }
 })
 
