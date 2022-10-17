d3d4cebc025d5f69730b9558be92ae466904b345
Adil Saju <adilsaju@gmail.com>
2022-10-16 23:53:15 -0700

    refactor bkend

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -26,37 +26,37 @@ router.get('/students/:id',async (req,res)=>{
 })
 
 
-//getting all requests
-router.get('/requests',async (req,res)=>{
-    try {
-        const abc=await Request.requestModel.find()
-        res.json(abc)
-    } catch (error) {
-        res.status(500).json({ message: error.message})
-    }
-})
+// //getting all requests
+// router.get('/requests',async (req,res)=>{
+//     try {
+//         const abc=await Request.requestModel.find()
+//         res.json(abc)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message})
+//     }
+// })
 
 
-//getting request by id
-router.get('/requests/:id',async (req,res)=>{
-    try {
-        //get requests column from student
-        const abc=await Request.requestModel.find(req.params.id)
-        res.json(abc)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+// //getting request by id
+// router.get('/requests/:id',async (req,res)=>{
+//     try {
+//         //get requests column from student
+//         const abc=await Request.requestModel.find(req.params.id)
+//         res.json(abc)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
 
 //getting all requests by a particular student
-// url: /requests?student=12324sadsa124223
+// url: /requests?student=634c84017abbf81281febf50
 //FOR travel order page in student UI
 router.get('/requests',async (req,res)=>{
     try {
         const studentId = req.query.student
         //get requests column from student
         // const abc=await Student.studentModel.findById(studentId).select('requests')
-
+        console.log("rrrr")
         const abc=await  Request.requestModel.find({ "requestedStudent._id":  studentId})
         
         res.json(abc)
