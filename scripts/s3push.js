3c8d55e9749237857765456a2d2c3997e2554f29
Adil Saju <adilsaju@gmail.com>
2022-10-21 08:46:22 -0700

    .

--- scripts/s3push.js
+++ scripts/s3push.js
@@ -1,11 +1,11 @@
 const { getStorage, ref, uploadBytesResumable, getDownloadURL } = require (  "firebase/storage" ) ;
 const app = require("../firebase")
-const fs = require('fs');
+// const fs = require('fs');
 
-let txtFile = "test.png";
-let str = fs.readFileSync(txtFile,'utf8');
+// let txtFile = "test.png";
+// let str = fs.readFileSync(txtFile,'utf8');
 
-console.log(str.name);
+// console.log(str.name);
 //FIXME: file object
 process.exit()
 
