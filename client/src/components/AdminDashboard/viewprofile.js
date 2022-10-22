9d590daa1e209756579bb5a3b2a3261296a01dac
Adil Saju <adilsaju@gmail.com>
2022-10-21 23:58:37 -0700

    PROFILE all good so faar

--- client/src/components/AdminDashboard/viewprofile.js
+++ client/src/components/AdminDashboard/viewprofile.js
@@ -6,8 +6,8 @@ import SideMenuAdmin from '../Navbar/SideMenuAdmin';
 import { useParams } from "react-router-dom";
 
 
-const fetchTasks = async () => {
-  let url = `/students/634c84017abbf81281febf50`;
+const fetchTasks = async (request_id) => {
+  let url = `/requests/${request_id}`;
   const res = await fetch(url);
   const data = await res.json();
 
@@ -33,13 +33,27 @@ const decline = async () => {
   return data;
 };
 
+const updateStudentNotes = async (students, newNote) => {
+  let url = `/students/${students.requestedStudent._id}`;
+  const bod1 = {
+    "notes": "asdsadasdsaads asdkldjalks"
+    }
+  const res = await fetch(url, {method: 'PATCH', body: JSON.stringify(bod1),     headers: {
+    'Content-Type': 'application/json'
+  }, });
+  const data = await res.json();
+
+  console.log("IMPPPPPPPPPPPPP:",data);
+  return data;
+};
+
 const Viewprofile = () => {
   const [students,setStudents] = useState([]);
   let params = useParams();
 
   useEffect(() => {
     const getTasks = async () => {
-      const tfs = await fetchTasks();
+      const tfs = await fetchTasks(params.id);
       setStudents(tfs);
     };
     getTasks();
@@ -48,8 +62,23 @@ const Viewprofile = () => {
   return (
     <div>
       <SideMenuAdmin/>
-      <h3>{students.name}</h3>
-      <h4>{students.email}</h4>
+      <h3>{students.requestedStudent && students.requestedStudent.name}</h3>
+      {/* <h4>{students.requestedStudent && students.requestedStudent.email}</h4> */}
+
+      <h4>student number: {students.requestedStudent && students.requestedStudent.studentNumber}</h4>
+      <h4>travel date: {students.requestedStudent && students.requestedStudent.flightDate}</h4>
+
+      <h4>current license: {students.requestedStudent && students.requestedStudent.studentRequirements.licenseType}</h4>
+      <h4>current program: {students.requestedStudent && students.requestedStudent.program}</h4>
+
+      <h4>Hours flown: {students.requestedStudent && students.requestedStudent.studentRequirements.flownHours}</h4>
+
+      <h4>License images will be shown here......</h4>
+
+      <textarea name="" id="" cols="30" rows="10" value={students.requestedStudent && students.requestedStudent.notes}></textarea> <br />
+      <button onClick={(e) => { updateStudentNotes(students, e.target.value)} }>Update Notes</button>
+
+
       <h4>{params.id}</h4>
       {/* <div>
       <Accept/>
