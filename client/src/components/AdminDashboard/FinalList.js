c0bd057ef3851deaaf9b5539e96909232940caa1
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-10-21 18:02:03 -0700

    final_list

--- client/src/components/AdminDashboard/FinalList.js
+++ client/src/components/AdminDashboard/FinalList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import SideMenuAdmin from '../Navbar/SideMenuAdmin';
+
+import { Link } from "react-router-dom";
+
 //Fetch Data using API
 const fetchTasks = async () => {
-    let url = `/students/633a0695b149556c00bfc725`;
+    let url = `/finalList`;
     const res = await fetch(url);
     const data = await res.json();
   
@@ -32,13 +35,47 @@ const fetchTasks = async () => {
               
               const isEmpty = Object.keys(students).length === 0;
               console.log(isEmpty);
+  
+              let count = 1;
     return (
-        <>
-        <SideMenuAdmin/>
-        <h2>In progress</h2>
-           {console.log("o teri") }
-        </>
+               
+        <div>
+            <SideMenuAdmin/>
      
-    )}
+            <>
+            <button > <Link to='/student-travel-order'>Send to Flight Coordinator</Link></button>
+            </>
+          
+   {students.map((student,id)=> {
+      
+      
+      return (
+        
+        <div key={id}>
+         <table>
+             <tbody>
+              <tr>
+                <td>{count++}</td>
+                <td>{ student.requestedStudent.name}</td>
+                <td>{ student.requestedStudent.studentNumber}</td>
+                <td>{student.flightDate}</td>
+                <td><Link to='/travel-order/profile'>View Profile</Link></td>
+              </tr>
+              </tbody>
+            </table>
+        </div>
+    )
+   } ,[])
+
+   
+   
+        }
+    
+        
+        </div> 
+        
+            )
+
+}
 
 export default FinalList
