8d3e2f3fc801d3faa8870be601dfe28a27bd0a15
Adil Saju <adilsaju@gmail.com>
2022-10-23 08:48:54 -0700

    upload UI done

--- client/src/components/StudentDashboard/UploadDocument.js
+++ client/src/components/StudentDashboard/UploadDocument.js
@@ -3,11 +3,17 @@ import React from 'react'
 
 const UploadDocument = () => {
   return (
-    <div>UploadDocument
+    <>
+    <h2>Upload Documents</h2>
 
+    <div><label htmlFor="l1">Medical License</label><input type="file" name="l1" id="l1" /></div><br />
+    <div><label htmlFor="l2">Private License</label><input type="file" name="l2" id="l2" /></div><br />
+    <div><label htmlFor="l3">Radio License</label><input type="file" name="l3" id="l3" /></div><br />
+    <div><label htmlFor="l4">English Proficiency</label><input type="file" name="l4" id="l4" /></div><br />
+    
 
-
-    </div>
+    <button>Upload</button>
+    </>
     
   )
 }
