19ce29d76bfde5b8094e54fc4d1c5ee471b88818
Kirito9988 <tarun.1999thakur33@gmail.com>
2022-10-26 21:03:00 -0700

    not found and fetch error handling done.

--- client/src/components/AdminDashboard/Search.jsx
+++ client/src/components/AdminDashboard/Search.jsx
@@ -2,51 +2,14 @@ import React from 'react'
 
 const Search = () => {
    
-      // const myFunction = () => {
-      //   console.log("getting calledddddddddddddddddddddd");
-      //   // Declare variables
-      //   let input, filter, table, tr, td, i, txtValue;
-      //   input = document.getElementById("myInput");
-      //   const msg = document.getElementById("msg");
-      
-      //   filter = input.value.toUpperCase();
-        
-        
-      //   table = document.getElementsByClassName("myTable");
-      //   console.log(table);
-      //   tr = document.getElementsByClassName("tay");
-      
-      //   // Loop through all table rows, and hide those who don't match the search query
-      //   const  tLen = tr.length
-      //   let emptyCnt = 0
-      //   for (i = 0; i < tr.length; i++) {
-      //     td = tr[i].getElementsByTagName("td")[2];
-      //     if (td) {
-      //       txtValue = td.textContent || td.innerText;
-      //       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      //         tr[i].style.display = "";
-      //       } else {
-      //         tr[i].style.display = "none";
-      //         emptyCnt ++;
-      
-      //       }
-      //     }
-      //   }
-      
-      //   if (emptyCnt === tLen){
-      //     msg.style.display = "";
-      
-      //   }else {
-      //     msg.style.display = "none";
-      
-      //   }
-      // }
       function searchSname() {
         var input, filter, found, table, tr, td, i, j;
         input = document.getElementById("myInput");
+        const msg = document.getElementById("msg");
         filter = input.value.toUpperCase();
         table = document.getElementsByClassName("myTable");
         tr = document.getElementsByClassName("tay");
+        let emptyCnt = 0
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td");
             for (j = 0; j < td.length; j++) {
@@ -59,9 +22,17 @@ const Search = () => {
                 found = false;
             } else {
                 tr[i].style.display = "none";
+                emptyCnt ++;
             }
         }
-    }
+    
+    if (emptyCnt === tr.length){
+          msg.style.display = "";
+      
+        }else {
+          msg.style.display = "none";
+        }
+        }
   return (
     <>
    <input type="text" id="myInput" onKeyUp={(e) => { searchSname()} }  placeholder="Search for names.." />
