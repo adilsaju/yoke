51dd3634338100024bb5d9ce6e8cec515152e2eb
Adil Saju <adilsaju@gmail.com>
2022-10-26 22:06:05 -0700

    data-create script upds

--- scripts/data-create.js
+++ scripts/data-create.js
@@ -2,6 +2,26 @@ const Student = require('../models/StudentModel')
 const Checklist = require('../models/checklistModel')
 const Request = require('../models/requestModel')
 const Admin = require('../models/adminModel')
+require('dotenv').config()
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+// const studentRoute = require('./routes/studentRoute')
+// const bodyParser = require('body-parser')
+
+// const login = require('./routes/login')
+// const errorHandler = require('./middlewares/errorMiddleware')
+// const jwt = require('jsonwebtoken')
+
+const port = process.env.PORT || 5001
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true } )
+const db = mongoose.connection 
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.error('connected to database'))
+
+
+
 
 console.log("rannnn data addd()")
 
@@ -11,21 +31,25 @@ async function addData() {
 
     const studentRequirements1 = await Checklist.studentRequirementsModel.create({flownHours: 123 , balance: 66, licenseType: "ykkkk", englishProficiency: true, medicalLicense: "abc", radioLicense: "abc", license: "ykkk"})
 
-
-
+    
     const admin1 = await Admin.adminModel.create({name: "Claire" , email: "claire@gmail.com", password: "12345678" })
-
+    
     const student1 = await Student.studentModel.create({name: "Jane" , email: "adilsaju@gmail.com", password: "12345678", studentNumber: 35209583920, photo: "assffsafsafsa", program: "cpl", studentRequirements: studentRequirements1 })
-
+    
     const student2 = await Student.studentModel.create({name: "Joanne" , email: "adilsaju@gmail.com", password: "12345678", studentNumber: 35209583921, photo: "assffsafsafsa", program: "cpl", studentRequirements: studentRequirements1 })
-
+    
     const request1 = await Request.requestModel.create({requestedStudent: student1,approvedAdmin: admin1})
     const request2 =await Request.requestModel.create({requestedStudent: student2,approvedAdmin: null})
     // user.name = ""
     // await user.save()
-
+    
     //const user  = new User({ name: "Kyle" , age: 26 })
     //await user.save()
+    Checklist.studentRequirementsModel.deleteMany({})
+    console.log("haha");
 }
+
+
+addData()
 
 exports.addData = addData
