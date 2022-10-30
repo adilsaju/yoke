24cf534066fe0a4b8c1ffae72df4912ab6996673
Claire Simbulan <elleclairesimbulan@gmail.com>
2022-10-30 11:51:54 -0700

    start the dougnut chart

--- client/src/components/AdminDashboard/Home.js
+++ client/src/components/AdminDashboard/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -8,10 +8,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 const { faker } = require('@faker-js/faker');
-
 
 ChartJS.register(
   CategoryScale,
@@ -19,7 +19,8 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ArcElement
 );
 
 export const options = {
@@ -35,39 +36,83 @@ export const options = {
   },
 };
 
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+];
 
 export const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      data: labels.map(() =>
+        faker.datatype.number({ min: 0, max: 1000 })
+      ),
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      data: labels.map(() =>
+        faker.datatype.number({ min: 0, max: 1000 })
+      ),
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
 };
 
+//doughnut - students per course
+export const optionsDougnut = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'No. of Students per Course',
+    },
+  },
+};
+
+export const dataDougnut = {
+  labels: [
+    'Private Pilot, Commercial Pilot, Instrument Rating',
+  ],
+  datasets: [
+    {
+      label: 'Private Pilot',
+      data: labels.map(() =>
+        faker.datatype.number({ min: 0, max: 1000 })
+      ),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
 
 const Home = () => {
   // return (
   //   <>
   //   <div>Charts Display Here</div>
-    
-    
 
   //   <Doughnut data={...} />
   //   </>
   // )
 
-  return <Bar options={options} data={data} />;
+  return (
+    <>
+      <Bar options={options} data={data} />;
+      <Doughnut
+        options={optionsDougnut}
+        data={dataDougnut}
+      />
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
