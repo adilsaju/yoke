fbcad8f9c90e41a48d972b731dabf9fe5bb6e0f2
Claire Simbulan <elleclairesimbulan@gmail.com>
2022-11-01 14:22:53 -0700

    fixed alignment of admin-header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 import { useState,useEffect, useContext } from 'react';
 import {UserContext} from '../Contexts/UserContext'
 import "./Header.css"
-const Header = () => {
-  const {pageTitle} = useContext(UserContext)
 
+const Header = () => {
+const {pageTitle} = useContext(UserContext);
 
   return (
     <div >
         <header>
-          <div className='logoo'>
-        <img src={require('../yokelg.png')} />
-        </div>
-        <div className='page-name'>
-        <h3>{pageTitle}</h3>
+          <div className='logo'>
+            <img src={require('./images/logoWhite.png')} alt='' />
+          </div>
+          <div className='page-name'>
+            <h3>{pageTitle}</h3>
         {/* <button > <Link to='/login'>Login Page</Link></button>
     <button > <Link to='/'>Admin Dashboard</Link></button>
     <button > <Link to='/student-account-status'>Student Dashboard</Link></button> */}
     
-        </div>
+          </div>
         </header>
     </div>
   )
