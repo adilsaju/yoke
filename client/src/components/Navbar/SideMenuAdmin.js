e0e9d573e212703281551da522be3a28fc65b0f4
Kirito9988 <tarun.1999thakur33@gmail.com>
2022-11-01 19:57:02 -0700

    NUmber of request displaying and now popup opening on decline

--- client/src/components/Navbar/SideMenuAdmin.js
+++ client/src/components/Navbar/SideMenuAdmin.js
@@ -1,7 +1,48 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useState,useEffect } from 'react';
 
 const SideMenuAdmin = () => {
+  const [requests,setRequests] = useState([]);
+  const [error, setError] = useState(null);
+  const [finalstudents,FinalStudents] = useState([]);
+
+  useEffect(() => {
+  setTimeout(() => {
+    fetch(`/pendingRequests`).then(res => {
+      if(!res.ok) {
+        throw Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then(data => {
+      console.log("TOOO: data");
+      setRequests(data);
+      setError(null);
+    }).catch(err => {
+      setError(err.message)
+    })
+   },1000)
+  
+      }, []);
+
+      useEffect(() => {
+        setTimeout(() => {
+         fetch(`/finalList`).then(res => {
+           if(!res.ok) {
+             throw Error(res.statusText);
+           }
+           return res.json();
+         })
+         .then(data => {
+           FinalStudents(data);
+           setError(null);
+         }).catch(err => {
+           setError(err.message)
+         })
+        },1000)
+
+    }, []);
   return (
     <div className='Nav-menu'>
   
@@ -12,14 +53,14 @@ const SideMenuAdmin = () => {
         </li></Link>
         
         <Link to="/travel-order"> <li>
-          Travel Order
+          Travel Order({(requests.length)})
         </li></Link>
         {/* <li>
         <Link to="/login">Login</Link>
         </li> */}
        
        <Link to="/final-list"> <li>
-          Final list
+          Final list({(finalstudents.length)})
         </li></Link>
 
 
