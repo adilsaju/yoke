4a7658cd66f298c564d8115d37422d2743652973
Claire Simbulan <elleclairesimbulan@gmail.com>
2022-11-02 15:36:59 -0700

    student profile under acct status done

--- client/src/components/StudentDashboard/studentUpload.js
+++ client/src/components/StudentDashboard/studentUpload.js
@@ -1,49 +1,53 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+// import './studentAccountStatus.css';
 
 const StudentUpload = (props) => {
     
     
   return (
-    <div>
+    <div className="whiteBg">
         
         <div className='studentupload'>
-          <h1> Uploaded documents :</h1>
-          <button > <Link to='/student-account-status/upload-document'>Update</Link></button>
-        </div>
+          <div className="topOfTheBox">
+            <h3>Uploaded documents</h3>
+            <button className="yellowBtn"> <Link to='/student-account-status/upload-document'>Update</Link></button>
+          </div>
+        
 
-        <div className='imageupload'>
-          <h2>
-              Medical License: <br></br>
-              <img src={props.starry && props.starry.studentRequirements && props.starry.studentRequirements.medicalLicense}></img>
-          </h2>
-          
-          <br>
-          </br>
-          <h2>
-              Private License: <br></br>
-              <img src= {props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.license}></img>
-          
-               {/* {props.starry.studentDocumentVerification.license} */}
-          </h2>
-          <br>
-          </br>
-          <h2>
-              Radio License:<br></br>
-          
-              <img src={props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.radioLicense}></img>
-               {/* {props.starry.studentDocumentVerification.radioLicense} */}
-          </h2>
-          
-          <br>
-          </br>
-          
-          <h2>
-              English Proficiency :
-              <br></br>
-              <img src={props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.englishProficiency}></img>
-          </h2>
-        </div>
+            <div className='imageupload'>
+              <div>
+                <h3>Medical License</h3>
+                  <img src={props.starry && props.starry.studentRequirements && props.starry.studentRequirements.medicalLicense} />
+              </div>
+              
+              
+                
+                <div>
+                  <h3>Private License</h3>
+                    <img src= {props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.license} />
+                </div>
+                
+                    {/* {props.starry.studentDocumentVerification.license} */}
+                
+
+                <div>
+                  <h3>Radio License</h3>
+                    <img src={props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.radioLicense} />
+                </div>
+                    {/* {props.starry.studentDocumentVerification.radioLicense} */}
+                
+                
+                <div>
+                  <h3>English Proficiency </h3>
+                    <img src={props.starry && props.starry.studentRequirements &&  props.starry.studentRequirements.englishProficiency} />
+                </div>
+                
+            </div>
+            {/* end of imageupload */}
+        </div>   
+        {/* end of studentupload  */}
+
        <br></br>
         
    
