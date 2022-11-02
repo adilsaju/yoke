093577160015bee6b8069cc83d27815e5877f421
Adil Saju <adilsaju@gmail.com>
2022-11-02 09:34:15 -0700

    setting popup done

--- client/src/components/AdminDashboard/Setting.jsx
+++ client/src/components/AdminDashboard/Setting.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState,useEffect,useContext } from 'react';
 import { UserContext } from '../../Contexts/UserContext'; 
 import SideMenuAdmin from '../Navbar/SideMenuAdmin';
+import Modal from 'react-modal';
+
 
 const fetchTasks = async (admin_id) => {
   let url = `/admins/${admin_id}`;
@@ -12,7 +14,36 @@ const fetchTasks = async (admin_id) => {
   return data;
 };
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 2
+  },
+};
+Modal.setAppElement('#root');
+
+
 const Setting = () => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+
+
   const {pageTitle, setPageTitle} = useContext(UserContext)
  
         const [admin,setAdmin] = useState([]);
@@ -40,11 +71,32 @@ const Setting = () => {
               { <><h3>Email: <span className="fontFira">{admin.email}</span></h3>
               <h4>Password: <span className="maskPw">{admin.password}</span></h4></> }
               <div>
-                <button className="dBlueBtn">Forgot Password</button>
+                <button className="dBlueBtn" onClick={openModal} >Forgot Password</button>
               </div>
             </div>
         </div>
       </div>
+
+
+    {/* ============= modal ================== */}
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>OK</h2>
+        <button onClick={closeModal}>close</button>
+        <div>Password reset email has been successfully sent to your email id.</div>
+        {/* <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form> */}
+      </Modal>
     </>
   )
 }
