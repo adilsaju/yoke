5f20b34c001b9045fea801c6bfd9d10faa56ba48
Claire Simbulan <elleclairesimbulan@gmail.com>
2022-11-04 15:51:21 -0700

    hide header and footer on login and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom";
 import { useState,useEffect, useContext } from 'react';
 import {UserContext} from '../Contexts/UserContext'
 import './HeaderFooter.css'
+import { useLocation } from "react-router-dom";
+
 
 const Header = () => {
 const {pageTitle} = useContext(UserContext);
+
+const { pathname } = useLocation();
+  console.log(pathname);
+  if (pathname === "/login" || "/logout") return null;
+
 
   return (
     <div >
