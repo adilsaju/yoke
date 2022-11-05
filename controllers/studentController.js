618cf367f025fa2118d02b1005ff3b4f4f618ed6
Adil Saju <adilsaju@gmail.com>
2022-11-04 17:49:02 -0700

    archive done fix

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -634,7 +634,7 @@ const archive = () => {
             },
           ],
         })
-        .sort({ flightDate: 1, requestedDate: 1 });
+        .sort({ flightDate: 1, requestedDate: 1 }).populate("requestedStudent").populate("approvedAdmin");
 
       res.json(archivedRequests);
     } catch (error) {
