64eda53725dbe69ac331abfc668f26941b17dd9f
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-11-13 21:15:27 -0800

    sidemenu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,18 +5,40 @@ import {UserContext} from '../Contexts/UserContext'
 import './HeaderFooter.css'
 import { useLocation } from "react-router-dom";
 
+let navToggle = document.querySelector('.toggle-nav');
+
+const define = () => {
+  navToggle = document.querySelector('.toggle-nav');
+ 
+
+
+if(navToggle){
+navToggle.addEventListener('click', () => {
+  console.log('clickme')
+  document.body.classList.toggle('nav-open');
+});
+}
+} 
+
+
+// }
 
 const Header = () => {
 const {pageTitle} = useContext(UserContext);
 
 const { pathname } = useLocation();
   console.log(pathname);
+
   if (pathname === "/login" || pathname === "/logout") return null;
 
 
   return (
     <div >
+      
         <header>
+        <div className='hamburg'>
+            <button onFocus={define} className='toggle-nav'>///</button>
+          </div>
           <div className='logo'>
             <img src={require('./images/logoWhite.png')} alt='' />
           </div>
