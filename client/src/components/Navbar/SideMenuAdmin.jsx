bb3eb8592e92585606d126e2e93d96f311fbc1f5
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-11-15 15:40:22 -0800

    admin sidemenu

--- client/src/components/Navbar/SideMenuAdmin.jsx
+++ client/src/components/Navbar/SideMenuAdmin.jsx
@@ -3,7 +3,22 @@ import { Link } from "react-router-dom";
 import { useState,useEffect } from 'react';
 import './sideMenu.css';
 
-
+const define = () => {
+  console.log('init')
+//   let close = true;
+//   navToggle = document.querySelector('.toggle-nav');
+//   console.log(navToggle)
+// navToggle && navToggle.addEventListener('click', () => {
+//   console.log('clickme')
+//   if(close){
+//     close = false
+//     document.body.classList.add('nav-open');
+//   }else{
+//     close = true
+//   document.body.classList.remove('nav-open');
+// }
+// });
+}
 const SideMenuAdmin = () => {
 
   const [requests,setRequests] = useState([]);
@@ -48,7 +63,13 @@ const SideMenuAdmin = () => {
 
   return (
     <div className='Nav-menu'>
-  
+  <div className="mobmen">
+      
+      {define()}
+<div className='logomob'><img src={require('../images/logoWhite.png')} alt='Yoke' /></div>
+<div onClick={define()} className='logomob'> <button  className='toggle-nav logomob'><img src={require('../images/logo-cros.png')} alt='Yoke' /></button></div>
+{/* <div className='logocros'><img src={require('../images/logo-cros.png')} alt='Yoke' /></div> */}
+</div>
       <nav>
       <ul>
         
