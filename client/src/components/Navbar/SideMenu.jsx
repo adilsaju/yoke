b3350b9b3c2de25e4c899d252a81dc741347371b
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-11-15 15:35:52 -0800

    fix menu

--- client/src/components/Navbar/SideMenu.jsx
+++ client/src/components/Navbar/SideMenu.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import './sideMenu.css';
 
+let navToggle = document.querySelector('.toggle-nav');
+
+const define = () => {
+  console.log('init')
+//   let close = true;
+//   navToggle = document.querySelector('.toggle-nav');
+//   console.log(navToggle)
+// navToggle && navToggle.addEventListener('click', () => {
+//   console.log('clickme')
+//   if(close){
+//     close = false
+//     document.body.classList.add('nav-open');
+//   }else{
+//     close = true
+//   document.body.classList.remove('nav-open');
+// }
+// });
+}
+
 const SideMenu = () => {
   return (
     <div className='Nav-menu'>
+      <div className="mobmen">
+      
+            {define()}
+      <div className='logomob'><img src={require('../images/logoWhite.png')} alt='Yoke' /></div>
+      <div onClick={define()} className='logomob'> <button  className='toggle-nav logomob'><img src={require('../images/logo-cros.png')} alt='Yoke' /></button></div>
+      {/* <div className='logocros'><img src={require('../images/logo-cros.png')} alt='Yoke' /></div> */}
+      </div>
+
+      
   <nav>
+    
+  
       <ul className="studentMenu">
         <Link to="/student-account-status">
           <li className='studentAcct'>
