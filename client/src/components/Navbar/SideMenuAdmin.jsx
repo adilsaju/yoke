8bd00e8a62628cafb9aa36559e0cfa7b3add7afa
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-11-15 16:50:24 -0800

    admin side menu mobile fix

--- client/src/components/Navbar/SideMenuAdmin.jsx
+++ client/src/components/Navbar/SideMenuAdmin.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { useState,useEffect } from 'react';
 import './sideMenu.css';
+import '../HeaderFooter.css'
 
+let navToggle = document.querySelector('.crosbtn');
 const define = () => {
-  console.log('init')
-//   let close = true;
-//   navToggle = document.querySelector('.toggle-nav');
-//   console.log(navToggle)
-// navToggle && navToggle.addEventListener('click', () => {
-//   console.log('clickme')
-//   if(close){
-//     close = false
-//     document.body.classList.add('nav-open');
-//   }else{
-//     close = true
-//   document.body.classList.remove('nav-open');
-// }
-// });
+
+  
+  navToggle = document.querySelector('.crosbtn');
+  console.log(navToggle)
+navToggle && navToggle.addEventListener('click', () => {
+  console.log('clickme')
+
+    document.body.classList.remove('nav-open');
+ 
+});
+}
+
+const closemenuonclick = () => {
+
+
+    document.body.classList.remove('nav-open');
+ 
+
 }
 const SideMenuAdmin = () => {
 
@@ -67,17 +73,17 @@ const SideMenuAdmin = () => {
       
       {define()}
 <div className='logomob'><img src={require('../images/logoWhite.png')} alt='Yoke' /></div>
-<div onClick={define()} className='logomob'> <button  className='toggle-nav logomob'><img src={require('../images/logo-cros.png')} alt='Yoke' /></button></div>
-{/* <div className='logocros'><img src={require('../images/logo-cros.png')} alt='Yoke' /></div> */}
+<button onClick={(e) => { define()}} className='crosbtn'><img src={require('../images/logo-cros.png')} alt='Yoke' /></button>
+      {define()}
 </div>
       <nav>
       <ul>
         
-      <Link to="/"><li className='dashboard'>
+      <Link to="/"><li onClick={(e) => { closemenuonclick()}} className='dashboard'>
           Dashboard
       </li></Link>
         
-        <Link to="/travel-order"> <li className="travelOrder">
+        <Link to="/travel-order"> <li onClick={(e) => { closemenuonclick()}} className="travelOrder">
           Travel Order&nbsp;
           <button className='countInd'><span>{(requests.length)}</span></button>
         </li></Link>
@@ -85,22 +91,22 @@ const SideMenuAdmin = () => {
         <Link to="/login">Login</Link>
         </li> */}
        
-       <Link to="/final-list"> <li className="finalList">
+       <Link to="/final-list"> <li onClick={(e) => { closemenuonclick()}} className="finalList">
           Final list&nbsp;
           <button className='countInd'><span>{finalstudents.length}</span></button>
         </li></Link>
 
 
-      <Link to="/history"><li className='archive'>
+      <Link to="/history"><li onClick={(e) => { closemenuonclick()}} className='archive'>
          History
       </li></Link>
 
         <div className='setandlog'>
-          <Link to="/setting"><li className='settings'>
+          <Link to="/setting"><li onClick={(e) => { closemenuonclick()}} className='settings'>
           Setting
             </li></Link>
 
-           <Link to="/logout"><li className='logout'>
+           <Link to="/logout"><li onClick={(e) => { closemenuonclick()}} className='logout'>
           Logout
            </li></Link>
   
