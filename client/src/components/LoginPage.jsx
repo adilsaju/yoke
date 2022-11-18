f8b4d5c96163ad847a199b801838e7d58dce9feb
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-11-18 04:05:45 -0800

    login fail handled

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -10,9 +10,20 @@ import pwEye1 from  './images/pwHideDark.svg';
 import pwEye2 from  './images/pwShowDark.svg';
 
 
+const define = () => {
+  
+  document.body.classList.add('loginerrorhide');
+
+}
+
+const defineji = () => {
+  document.body.classList.remove('loginerrorhide');
+}
+
 
 const LoginPage = () => {
   // const [request,setStudents] = useState([]);
+  document.body.classList.remove('loginerrorhide');
   const { loginCredentials, setLoginCredentials } = useContext(UserContext)
   const {pageTitle, setPageTitle} = useContext(UserContext)
 
@@ -42,6 +53,7 @@ const LoginPage = () => {
     console.log("IMPPPPPPPPPPPPP:",data);
     if (data.error === false){
       console.log("succesfooly logged in")
+      defineji()
       //save id and token globally
       // setIsLoggedIn(true);
       console.log(data.isAdmin);
@@ -83,7 +95,7 @@ const LoginPage = () => {
 
     }else{
       console.log("unsuccess")
-      alert("unsuccess")
+      define()
     }
   
     return data;
@@ -157,9 +169,9 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-
+      <span className='loginerror'>Incorrect username or password.</span>
         <br />
-        <br />
+     
       
       <button className="yellowBtn" onClick={(e) => { login()}} >Login</button>
       
