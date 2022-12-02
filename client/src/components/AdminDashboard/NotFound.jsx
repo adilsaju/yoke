02bfd8da35a3b958bbff8d45d6c7843256e38906
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-12-01 23:51:08 -0800

    not found page fix

--- client/src/components/AdminDashboard/NotFound.jsx
+++ client/src/components/AdminDashboard/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className='not-found'>
    <h2>Sorry</h2>
     <p>Page Cannot be found</p>
-   <Link to="/">To Home Page</Link>
+   <Link to="/"><button className='dBlueBtn'> Go to Home Page</button></Link>
     </div>
  
   )
